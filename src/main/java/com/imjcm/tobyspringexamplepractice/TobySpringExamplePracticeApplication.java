b6ce1c553da574dd3d039472e72a00fb7f15a230
ImJcm <wjsckdals0909@naver.com>
2024-05-01 03:36:49 +0900

    Initial setting commit

---
+++ src/main/java/com/imjcm/tobyspringexamplepractice/TobySpringExamplePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.imjcm.tobyspringexamplepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringExamplePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobySpringExamplePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imjcm/tobyspringexamplepractice/TobySpringExamplePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imjcm.tobyspringexamplepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringExamplePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

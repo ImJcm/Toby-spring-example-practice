33e79e0e8dbf31a70e2a236b464a13dc491ab386
ImJcm <wjsckdals0909@naver.com>
2024-05-02 01:19:02 +0900

    create user class & create user table SQL

---
+++ src/main/java/com/imjcm/tobyspringexamplepractice/user/domain/User.java
@@ -0,0 +1,12 @@
+package com.imjcm.tobyspringexamplepractice.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    String id;
+    String name;
+    String password;
+}
